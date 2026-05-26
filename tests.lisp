44432efe5304a5b17b6a70167d0ec85764e880cc
Lovebird Connoisseur <90262313+Lovebird-Connoisseur@users.noreply.github.com>
2026-05-26 22:34:39 +0100

    tests.lisp: change equality comparator to one compatible with fset2 data structures

--- tests.lisp
+++ tests.lisp
@@ -17,57 +17,57 @@
 
 (def-suite* tag :in test-suite)
 
-;; TODO: Is equal the right function to compare maps?
+
 (test new-tag
   (let* ((new-field 'new-field)
          (field-value "New field value")
          (new-tag-list (goldentag:tag +default-tag-list+ new-field field-value))
          (new-field-value (fset2:lookup new-tag-list new-field)))
-    (is (equal new-field-value field-value))))
+    (is (fset2:equal? new-field-value field-value))))
 
 (test tag-already-exists
   (let* ((field-value "New value")
          (field 'existing-field)
          (new-tag-list (goldentag:tag +default-tag-list+ field field-value)))
-    (is (equal +default-tag-list+ new-tag-list))))
+    (is (fset2:equal? +default-tag-list+ new-tag-list))))
 
 (test tag-append
   (let* ((existing-field 'author)
          (new-value "Hunter")
          (expected-result (append (fset2:lookup +default-tag-list+ existing-field) (list new-value)))
          (new-tag-list (goldentag:tag +default-tag-list+ existing-field new-value :append t)))
-    (is (equal expected-result (fset2:lookup new-tag-list existing-field)))))
+    (is (fset2:equal? expected-result (fset2:lookup new-tag-list existing-field)))))
 
 (test tag-append-non-existant
   (let* ((non-existing-field 'non-existing-field)
          (new-value "Some Value")
          (expected-result new-value)
          (new-tag-list (goldentag:tag +default-tag-list+ non-existing-field new-value :append t)))
-    (is (equal expected-result (fset2:lookup new-tag-list non-existing-field)))))
+    (is (fset2:equal? expected-result (fset2:lookup new-tag-list non-existing-field)))))
 
 (test tag-overwrite
   (let* ((existing-field 'existing-field)
          (new-value "New Value")
          (new-tag-list (goldentag:tag +default-tag-list+ existing-field new-value :overwrite t)))
-    (is (equal (fset2:lookup new-tag-list existing-field) new-value))))
+    (is (fset2:equal? (fset2:lookup new-tag-list existing-field) new-value))))
 
 (test tag-overwrite-non-existant
   (let* ((non-existing-field 'existing-field)
          (new-value "New Value")
          (new-tag-list (goldentag:tag +default-tag-list+ non-existing-field new-value :overwrite t)))
-    (is (equal (fset2:lookup new-tag-list non-existing-field) new-value))))
+    (is (fset2:equal? (fset2:lookup new-tag-list non-existing-field) new-value))))
 
 (test tag-append-overwrite
   (let* ((existing-field 'author)
          (new-value "Hunter")
          (new-tag-list (goldentag:tag +default-tag-list+ existing-field new-value :append t :overwrite t)))
-    (is (equal (fset2:lookup new-tag-list existing-field) new-value))))
+    (is (fset2:equal? (fset2:lookup new-tag-list existing-field) new-value))))
 
 (test tag-append-overwrite-non-existant
   (let* ((non-existing-field 'non-existing-field)
          (new-value "Hunter")
          (new-tag-list (goldentag:tag +default-tag-list+ non-existing-field new-value :append t :overwrite t)))
-    (is (equal (fset2:lookup new-tag-list non-existing-field) new-value))))
+    (is (fset2:equal? (fset2:lookup new-tag-list non-existing-field) new-value))))
 
 (test untag)
 (test untag-non-existent)
